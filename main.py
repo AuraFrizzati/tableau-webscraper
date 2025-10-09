53a4e33cfdcf55340af526ba4999f1d02ce6f3f0
AuraFrizzati <frizzatia@gmail.com>
2025-10-09 19:30:20 +0100

    updated

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
 
 # --- Setup Headless Chrome ---
 # Create an instance of ChromeOptions
@@ -17,7 +21,7 @@ driver = webdriver.Chrome(options=chrome_options)
 # --- Open the Website ---
 # url = "https://automationintesting.com/selenium/testpage/"
 url = "https://public.tableau.com/app/profile/victoria6405/viz/WalesHCAISurveillanceMonthlyDashboard/HBMonthlyDashboard"
-
+url = "https://www2.nphs.wales.nhs.uk/WHAIPDocs.nsf"
 try:
     print(f"Opening {url} in headless mode...")
     driver.get(url)
@@ -26,19 +30,13 @@ try:
     print(f"Page title: '{driver.title}'")
     print("Successfully opened the page.")
 
-        # Print all HTML code from the page
+    # Print all HTML code from the page
     print("\n--- HTML Source Code ---")
     print(driver.page_source)
     print("------------------------")
 
 
+
 except Exception as e:
     print(f"An error occurred: {e}")
 
-finally:
-    # --- Close the Driver ---
-    # It's important to quit the driver to close the browser instance
-    # and free up system resources.
-    if driver:
-        driver.quit()
-        print("Browser closed.")
